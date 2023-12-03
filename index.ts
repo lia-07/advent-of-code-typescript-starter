06328cd2e2fa063f04f8f0f7dfb939cc454f3767
lia-07 <107384739+lia-07@users.noreply.github.com>
2023-12-03 21:35:16 +1300

    make some major changes

--- index.ts
+++ index.ts
@@ -5,17 +5,22 @@ import { join } from "path";
 
 const sessionCookie = process.env.SESSION_COOKIE;
 
-switch (Bun.argv[2]) {
-  case "new":
-    scaffold(Number(Bun?.argv[3]), Number(Bun?.argv[4]));
-    break;
-  default:
-    console.error("...What am I supposed to do?");
-    break;
-}
+scaffold(Number(Bun?.argv[2]), Number(Bun?.argv[3]));
 
 async function scaffold(year?: number, day?: number): Promise<void> {
+  // if unspecified, fill in year and day
   if (!day && !year) {
+    // make sure it's december est if day and year aren't provided
+    if (
+      new Date().toLocaleString("en-US", {
+        timeZone: "EST",
+        month: "numeric",
+      }) !== "12"
+    ) {
+      console.error("❌ Current month (EST)/month provided is not December.");
+      return;
+    }
+
     day = Number(
       new Date().toLocaleString("en-US", {
         timeZone: "EST",
@@ -31,20 +36,9 @@ async function scaffold(year?: number, day?: number): Promise<void> {
     );
   }
 
-  // make sure it's december EST if day and year aren't provided
-  if (
-    new Date().toLocaleString("en-US", {
-      timeZone: "EST",
-      month: "numeric",
-    }) !== "12"
-  ) {
-    console.error("Current month (EST)/month provided is not December.");
-    return;
-  }
-
   // if the year is invalid
   if (year! > new Date().getFullYear() || year! < 2015) {
-    console.error("Current year/year provided is invalid.");
+    console.error("❌ Current year/year provided is invalid.");
     return;
   }
 
@@ -61,12 +55,19 @@ async function scaffold(year?: number, day?: number): Promise<void> {
           })
         ))
   ) {
-    console.error("Current day/day provided is invalid.");
+    console.error("❌ Current day/day provided is invalid.");
     return;
   }
 
   const dayPath = join(String(year), `day-${String(day).padStart(2, "0")}`);
-  const solutionPath = join(dayPath, "solution.ts");
+
+  const part1Path = join(dayPath, "part-1.ts");
+  const part2Path = join(dayPath, "part-2.ts");
+
+  const inputPath = join(dayPath, "in.txt");
+
+  const test1Path = join(dayPath, "part-1.test.ts");
+  const test2Path = join(dayPath, "part-2.test.ts");
 
   console.info(`\n🎄 Scaffolding for ${year} Advent of Code day ${day}...\n`);
   // create the files and directories
@@ -75,83 +76,130 @@ async function scaffold(year?: number, day?: number): Promise<void> {
       recursive: true,
     });
 
-    if (!existsSync(solutionPath)) {
+    // make the file for part 1 and it's test if they don't exist already
+    if (!existsSync(part1Path)) {
       writeFile(
-        solutionPath,
-
+        part1Path,
         `import { readFile } from "fs/promises";
 
 export function solve(input: string): any {
   // Your code goes here...
-  
 }
 
+// so bun test doesn't execute it (i know, right?)
 if (Bun.argv[2] === "solve") {
-  readFile(Bun.argv[3], "utf-8")
+  readFile("in.txt", "utf-8")
     .then((inputFile) => {
-      solve(inputFile);
+      console.log(solve(inputFile));
     })
     .catch((error) => {
-      console.error("Error reading input: " + error);
+      console.error("❌ " + error);
     });
 }
-
 `
       );
     }
+    if (!existsSync(test1Path)) {
+      writeFile(
+        test1Path,
+        `import { expect, test } from "bun:test";
 
-    writeFile(
-      join(dayPath, "solution.test.ts"),
-      `import { expect, test } from "bun:test";
-
-import { solve } from "./solution";
+import { solve } from "./part-1";
 
 // Fill in the test data below
-
 const testInput = \`\`;
 
 const testAnswer = ;
 
-test("Provided Example", () => {
+test("Part 1", () => {
   expect(solve(testInput)).toBe(testAnswer);
 });
-    `
-    );
-
-    // if the user didn't provide the cookie, don't provide input
-    const inputPath = join(dayPath, "input.txt");
-
-    if (sessionCookie && !existsSync(inputPath)) {
-      console.info("ℹ️  Downloading input...");
-
-      await fetch(`https://adventofcode.com/${year}/day/${day}/input`, {
-        headers: {
-          Cookie: `session=${sessionCookie}`,
-          "User-Agent":
-            "https://github.com/lia-07/advent-of-code-typescript-starter by lia-8629@proton.me",
-        },
-      })
-        .then(async (i) => {
-          writeFile(inputPath, await i.text());
-          console.info("✅ ...Done.\n");
-        })
-        .catch((error) => console.error(error));
-    } else if (sessionCookie && existsSync(inputPath)) {
-      console.info("ℹ️  Input file already exists. Skipping input download.\n");
-    } else if (!sessionCookie && !existsSync(inputPath)) {
-      console.info(
-        "ℹ️  Session cookie was not provided. Skipping input download.\n"
+`
       );
-      writeFile(inputPath, "");
-    } else {
-      console.info(
-        "ℹ️  Session cookie was not provided, and input file already exists. Skipping download.\n"
+    }
+
+    // i think mac os blocked it for opening files too quickly idk
+    await new Promise((r) => setTimeout(r, 200));
+
+    // make the file for part 2 and it's test if they don't exist already
+    if (!existsSync(part2Path)) {
+      writeFile(
+        part2Path,
+        `import { readFile } from "fs/promises";
+
+export function solve(input: string): any {
+  // Your code goes here...
+}
+
+// so bun test doesn't execute it (i know, right?)
+if (Bun.argv[2] === "solve") {
+  readFile("in.txt", "utf-8")
+    .then((inputFile) => {
+      console.log(solve(inputFile));
+    })
+    .catch((error) => {
+      console.error("❌ Error reading input: " + error);
+    });
+}
+`
+      );
+    }
+    if (!existsSync(test2Path)) {
+      writeFile(
+        test2Path,
+        `import { expect, test } from "bun:test";
+
+import { solve } from "./part-2";
+
+// Fill in the test data below
+const testInput = \`\`;
+
+const testAnswer = ;
+
+test("Part 2", () => {
+  expect(solve(testInput)).toBe(testAnswer);
+});
+`
       );
     }
   } catch (error: any) {
-    console.error(`Error: ${error.message}`);
+    console.error(`❌ ${error.message}`);
     return;
   }
+
+  // if the user provided their session cookie, download the input file
+  // (also check it hasn't already been downloaded)
+  if (sessionCookie && !existsSync(inputPath)) {
+    console.info("⬇️  Downloading input file...");
+
+    await fetch(`https://adventofcode.com/${year}/day/${day}/input`, {
+      headers: {
+        Cookie: `session=${sessionCookie}`,
+        "User-Agent":
+          "https://github.com/lia-07/advent-of-code-typescript-starter by lia-8629@proton.me",
+      },
+    })
+      .then(async (i) => {
+        writeFile(inputPath, await i.text());
+        console.info("✅ ...Done.\n");
+      })
+      .catch((error) => {
+        console.error("❌ " + error + "\n");
+        console.info("💡 Download failed. Skipping input download.\n");
+      });
+  } else if (sessionCookie && existsSync(inputPath)) {
+    console.info("💡 Input file already exists. Skipping input download.\n");
+  } else if (!sessionCookie && !existsSync(inputPath)) {
+    console.info(
+      "💡 Session cookie was not provided. Skipping input download.\n"
+    );
+    writeFile(inputPath, "");
+  } else {
+    console.info(
+      "💡 Session cookie was not provided, and input file already exists. Skipping download.\n"
+    );
+  }
+
   console.info("🎅 Finished successfully.\n");
   console.info(`✨ cd ${dayPath} to continue.\n`);
 }
